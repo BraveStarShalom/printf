8c366d9d4abe8b909f8674ea726713f5d4a52fa9
BraveStarShalom <jesway1030@gmail.com>
2023-05-28 01:46:26 -0700

    header file updated

--- main.h
+++ main.h
@@ -43,6 +43,7 @@ int handle_print(const char *fmt, int *ind, va_list list,
 /******************** FUNCTIONS ********************/
 
 /* Funtions to print chars and strings */
+int _putchar(char c);
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
